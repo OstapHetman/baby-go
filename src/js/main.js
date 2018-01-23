4dfedf9ef2cf25926b590c0e7e6bb86326e39a31
OstapHetman <prickly4528@gmail.com>
2018-01-23 16:47:31 +0200

    implement two products section

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,11 @@ jQuery(function() {
         jQuery('.target').hide();
         jQuery('#div' + $(this).attr('target')).css("display", "flex")
     });
+    jQuery('.showSingle1').click(function() {
+        $(".activeted1").removeClass("activeted1");
+        jQuery($(this).addClass("activeted1"));
+        jQuery('.target1').slideUp();
+        jQuery('.target1').hide();
+        jQuery('#div' + $(this).attr('target')).css("display", "flex")
+    });
 });
