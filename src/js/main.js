40dbf6dcfcbb3369433d058bede641807176686c
OstapHetman <prickly4528@gmail.com>
2018-01-25 15:35:17 +0200

    made corrections on mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 jQuery(function() {
-
     jQuery('.showSingle').click(function() {
         $(".activeted").removeClass("activeted");
         jQuery($(this).addClass("activeted"));
@@ -14,21 +13,6 @@ jQuery(function() {
         jQuery('.target1').hide();
         jQuery('#div' + $(this).attr('target')).css("display", "flex")
     });
-    jQuery('.showSingle2').click(function() {
-        $(".activeted2").removeClass("activeted2");
-        jQuery($(this).addClass("activeted2"));
-        jQuery('.target2').slideUp();
-        jQuery('.target2').hide();
-        jQuery('#div' + $(this).attr('target')).css("display", "flex")
-    });
-    jQuery('.showSingle3').click(function() {
-        $(".activeted3").removeClass("activeted3");
-        jQuery($(this).addClass("activeted3"));
-        jQuery('.target3').slideUp();
-        jQuery('.target3').hide();
-        jQuery('#div' + $(this).attr('target')).css("display", "flex")
-    });
-
     jQuery('.showSingle4').click(function() {
         $(".activeted4").removeClass("activeted4");
         jQuery($(this).addClass("activeted4"));
