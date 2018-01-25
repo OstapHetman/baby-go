2a93e49aa5b85dde63176bb715d42880014fa5c5
OstapHetman <prickly4528@gmail.com>
2018-01-25 12:47:48 +0200

    imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    wait = require('gulp-wait');
+    wait = require('gulp-wait'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function() {
     return gulp.src('src/sass/**/*.scss')
@@ -12,5 +13,15 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
     gulp.watch('src/sass/**/*.scss', ['sass']);
 });
+// ImageMin
+gulp.task('imagemin', () =>
+    gulp.src('src/img/**/*.*')
+    .pipe(imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 })
+    ]))
+    .pipe(gulp.dest('dist/img'))
+);
 
 gulp.task('default', ['sass', 'watch']);
