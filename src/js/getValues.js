66517259720c7ecd563d22ddb4f02ea75086ce63
OstapHetman <prickly4528@gmail.com>
2018-02-02 15:36:59 +0200

    new images

--- src/js/getValues.js
+++ src/js/getValues.js
@@ -1,14 +1,18 @@
-let elements = document.getElementsByClassName('get'),
-    input = document.getElementById('hidden-input'),
-    selectInput = document.getElementById('select-input'),
-    dropdown = document.getElementById('dropdown');
+window.onload = function() {
+    let elements = document.getElementsByClassName('get'),
+        input = document.getElementById('hidden-input'),
+        selectInput = document.getElementById('select-input'),
+        dropdown = document.getElementById('dropdown'),
+        back = document.getElementsByClassName('back ')[0];
 
-for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].onclick = function() {
-        let a = this.getAttribute('data-value');
-        let b = dropdown.options[dropdown.selectedIndex].value;
-        selectInput.value = b;
-        input.value = a;
-        console.log(b);
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].onclick = function() {
+            let a = this.getAttribute('data-value');
+            let b = dropdown.options[dropdown.selectedIndex].value;
+            selectInput.value = b;
+            input.value = a;
+            console.log(b);
+        }
     }
-}
+    back.href = "javascript: history.go(-1)";
+};
