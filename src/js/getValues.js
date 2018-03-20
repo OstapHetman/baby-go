d5e77a63d1f8f2ed02ae82c1a89777ed1d40bcdd
OstapHetman <prickly4528@gmail.com>
2018-03-20 15:56:36 +0200

    endpoint

--- src/js/getValues.js
+++ src/js/getValues.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var elements = document.getElementsByClassName('get'),
+    var elements = document.getElementsByClassName('buy-btn'),
         input = document.getElementById('hidden-input'),
         selectInput = document.getElementById('select-input'),
         dropdown = document.getElementById('dropdown'),
@@ -14,20 +14,4 @@ window.onload = function() {
             console.log(b);
         }
     }
-    back.href = "javascript: history.go(-1)";
-    (function() {
-        var create = document.createElement('script');
-        var createAgain = document.createElement('script');
-        create.type = 'text/javascript';
-        create.async = true;
-        create.src = '../../js/online-call.js';
-
-        createAgain.type = 'text/javascript';
-        createAgain.async = true;
-        createAgain.src = '../../js/online-chat.js';
-        var onlineCall = document.getElementsByTagName('script')[2],
-            onlineChat = document.getElementsByTagName('script')[2];
-        onlineCall.parentNode.insertBefore(create, onlineCall);
-        onlineChat.parentNode.insertBefore(createAgain, onlineChat);
-    })();
 };
